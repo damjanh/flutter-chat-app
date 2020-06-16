daeff525747fd8ad348694a77d3a160e1312f11c
damjanh <damjan.hudobreznik@gmail.com>
2020-06-16 22:38:56 +0200

    Implement basic chat bubble.

---
+++ lib/widgets/message_bubble.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MessageBubble extends StatelessWidget {
+  final String message;
+
+  MessageBubble(this.message);
+
+  @override
+  Widget build(BuildContext context) {
+    return Wrap(
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            color: Theme.of(context).primaryColor,
+            borderRadius: BorderRadius.circular(12),
+          ),
+          width: 140,
+          padding: EdgeInsets.symmetric(
+            vertical: 10,
+            horizontal: 16,
+          ),
+          margin: EdgeInsets.symmetric(
+            vertical: 4,
+            horizontal: 8,
+          ),
+          child: Text(
+            message,
+            style: TextStyle(
+              color: Theme.of(context).accentTextTheme.headline1.color,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
