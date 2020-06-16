227aeb089feb5fa53cee7080156e3f00f51f3401
damjanh <damjan.hudobreznik@gmail.com>
2020-06-16 23:08:34 +0200

    Implement different style of chat message bubble depending on if the sender is current user.

--- lib/widgets/message_bubble.dart
+++ lib/widgets/message_bubble.dart
@@ -3,33 +3,51 @@ import 'package:flutter/material.dart';
 
 class MessageBubble extends StatelessWidget {
   final String message;
+  final bool isMe;
+  final Key key;
 
-  MessageBubble(this.message);
+  MessageBubble(
+    this.message,
+    this.isMe, {
+    this.key,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Wrap(
+    return Row(
+      mainAxisAlignment: isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
       children: [
-        Container(
-          decoration: BoxDecoration(
-            color: Theme.of(context).primaryColor,
-            borderRadius: BorderRadius.circular(12),
-          ),
-          width: 140,
-          padding: EdgeInsets.symmetric(
-            vertical: 10,
-            horizontal: 16,
-          ),
-          margin: EdgeInsets.symmetric(
-            vertical: 4,
-            horizontal: 8,
-          ),
-          child: Text(
-            message,
-            style: TextStyle(
-              color: Theme.of(context).accentTextTheme.headline1.color,
+        Wrap(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: isMe ? Colors.grey[300] : Theme.of(context).primaryColor,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(12),
+                  topRight: Radius.circular(12),
+                  bottomLeft: Radius.circular(!isMe ? 0 : 12),
+                  bottomRight: Radius.circular(isMe ? 0 : 12),
+                ),
+              ),
+              width: 140,
+              padding: EdgeInsets.symmetric(
+                vertical: 10,
+                horizontal: 16,
+              ),
+              margin: EdgeInsets.symmetric(
+                vertical: 4,
+                horizontal: 8,
+              ),
+              child: Text(
+                message,
+                style: TextStyle(
+                  color: isMe
+                      ? Colors.black
+                      : Theme.of(context).accentTextTheme.headline1.color,
+                ),
+              ),
             ),
-          ),
+          ],
         ),
       ],
     );
--- lib/widgets/messages.dart
+++ lib/widgets/messages.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 import '../widgets/message_bubble.dart';
@@ -6,26 +7,39 @@ import '../widgets/message_bubble.dart';
 class Messages extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder(
-      stream: Firestore.instance
-          .collection('chats')
-          .orderBy(
-            'createdAt',
-            descending: true,
-          )
-          .snapshots(),
-      builder: (ctx, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
+    return FutureBuilder(
+      future: FirebaseAuth.instance.currentUser(),
+      builder: (ctx, userSnapshot) {
+        if (userSnapshot.connectionState == ConnectionState.waiting) {
           return Center(
             child: CircularProgressIndicator(),
           );
         }
-        final chatDocs = snapshot.data.documents;
-        return ListView.builder(
-            reverse: true,
-            itemCount: chatDocs.length,
-            itemBuilder: (ctx, index) =>
-                MessageBubble(chatDocs[index]['text']));
+        return StreamBuilder(
+            stream: Firestore.instance
+                .collection('chats')
+                .orderBy(
+                  'createdAt',
+                  descending: true,
+                )
+                .snapshots(),
+            builder: (ctx, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+              final chatDocs = snapshot.data.documents;
+              return ListView.builder(
+                reverse: true,
+                itemCount: chatDocs.length,
+                itemBuilder: (ctx, index) => MessageBubble(
+                  chatDocs[index]['text'],
+                  chatDocs[index]['userId'] == userSnapshot.data.uid,
+                  key: ValueKey(chatDocs[index].documentID),
+                ),
+              );
+            });
       },
     );
   }
