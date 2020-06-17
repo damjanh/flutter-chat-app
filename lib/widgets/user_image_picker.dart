547db506040a88559974d7f23148446cad1223ab
damjanh <damjan.hudobreznik@gmail.com>
2020-06-17 09:45:56 +0200

    Implement user profile picture picker on auth screen when signing up.

---
+++ lib/widgets/user_image_picker.dart
@@ -0,0 +1,43 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class UserImagePicker extends StatefulWidget {
+  @override
+  _UserImagePickerState createState() => _UserImagePickerState();
+}
+
+class _UserImagePickerState extends State<UserImagePicker> {
+  File _pickedImage;
+
+  void _pickImage() async {
+    final picker = ImagePicker();
+    final pickedImage = await picker.getImage(source: ImageSource.gallery);
+    final pickedImageFile = File(pickedImage.path);
+
+    setState(() {
+      _pickedImage = pickedImageFile;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CircleAvatar(
+          backgroundColor: Colors.grey,
+          backgroundImage:
+              _pickedImage != null ? FileImage(_pickedImage) : null,
+          radius: 40,
+        ),
+        FlatButton.icon(
+          onPressed: _pickImage,
+          icon: Icon(Icons.image),
+          label: Text('Add Image'),
+          textColor: Theme.of(context).primaryColor,
+        ),
+      ],
+    );
+  }
+}
