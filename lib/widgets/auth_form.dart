0510770fd11d45b2ea2738c1e5e8626f905fb6c2
damjanh <damjan.hudobreznik@gmail.com>
2020-06-17 09:54:51 +0200

    Require user image for sign up on auth screen.

--- lib/widgets/auth_form.dart
+++ lib/widgets/auth_form.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 import 'user_image_picker.dart';
@@ -24,11 +26,26 @@ class _AuthFormState extends State<AuthForm> {
   String _userEmail = '';
   String _userName = '';
   String _userPassword = '';
+  File _userImageFile;
+
+  void _onImagePicked(File image) {
+    _userImageFile = image;
+  }
 
   void _trySubmit() {
     final isValid = _formKey.currentState.validate();
 
     FocusScope.of(context).unfocus();
+
+    if (!_isLogin && _userImageFile == null) {
+      Scaffold.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Please pick an image.'),
+          backgroundColor: Theme.of(context).errorColor,
+        ),
+      );
+      return;
+    }
 
     if (isValid) {
       _formKey.currentState.save();
@@ -55,7 +72,7 @@ class _AuthFormState extends State<AuthForm> {
               child: Column(
                 mainAxisSize: MainAxisSize.min,
                 children: [
-                  if (!_isLogin) UserImagePicker(),
+                  if (!_isLogin) UserImagePicker(_onImagePicked),
                   TextFormField(
                     key: ValueKey('email'),
                     validator: (value) {
--- lib/widgets/user_image_picker.dart
+++ lib/widgets/user_image_picker.dart
@@ -4,6 +4,10 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
 class UserImagePicker extends StatefulWidget {
+  final Function(File pickedImage) imagePickFn;
+
+  UserImagePicker(this.imagePickFn);
+
   @override
   _UserImagePickerState createState() => _UserImagePickerState();
 }
@@ -19,6 +23,8 @@ class _UserImagePickerState extends State<UserImagePicker> {
     setState(() {
       _pickedImage = pickedImageFile;
     });
+
+    widget.imagePickFn(pickedImageFile);
   }
 
   @override
